94f6c21abe3d49e8b82ba88638b73672c570e346
Marko Markovic <archorrs@gmail.com>
2022-06-02 14:21:05 +0200

    Update ControlButton.tsx

--- components/hms/ControlButton.tsx
+++ components/hms/ControlButton.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-interface ControlButtonProps {
+interface Props {
   active?: boolean;
   text: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   className?: string;  
-  children: React.ReactNode;
 }
 
-const ControlButton: React.FC<ControlButtonProps> = ({ active, text, children, onClick, className = '', ...props}) => {
+const ControlButton: React.FC<Props> = ({ active, text, children, onClick, className = '', ...props}) => {
   return (
     <div className="flex flex-col items-center">
       <button
@@ -25,4 +24,4 @@ const ControlButton: React.FC<ControlButtonProps> = ({ active, text, children, o
   );
 };
 
-export default ControlButton;
+export default ControlButton; 
